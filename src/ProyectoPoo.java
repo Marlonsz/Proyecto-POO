378dea6ef98c9b506953ec62ad795f3c6e727813
Piero Ulloa <ulloapiero@gmail.com>
2017-06-08 14:40:59 -0500

    Añadir lazo para que regrese al menú principal cuando hayan terminado todas las funciones. Arreglar salida propia del programa.

--- src/ProyectoPoo.java
+++ src/ProyectoPoo.java
@@ -10,55 +10,52 @@ public class ProyectoPoo {
     public static void main(String[] args) {
         //TODO: Terminar el main.
         Patio p_automotor = new Patio(15);
-        int respuesta = UI.imprimirMenuPrincipal();
-        System.out.println(respuesta);
         ArrayList<String> tipos = new ArrayList<>();
         tipos.add("automovil");
         tipos.add("camiones");
         tipos.add("camionetas");
         tipos.add("otros");
-        switch (respuesta) {
-            case 1:
-                UI.imprimirMenuIngresoVehicular();
-                String tipo = UI.ingresarTipoVehiculo(tipos);
-                switch (tipo) {
-                    case "automovil":
-                        ArrayList<Object> datos_comunes = UI.ingresoDatos();
-                        Automovil vh = UI.imprimirMenuIngresoAutomovil(datos_comunes);
-                        // TODO: Falta receptar el boolean para determinar si el ingreso del vh es exitoso.
-                        p_automotor.ingresarVehículo(vh);
-                        break;
-                    case "camiones":
-                        // Terminar el menu de camiones
-                        break;
-                    case "camionetas":
-                        // Terminar el menu de camionetas
-                        break;
-                    case "otros":
-                        // Terminar el menu para otros vehiculos
-                        break;
-                    default:
-                        // Asi vemos si las validaciones sirven.
-                        throw new IllegalArgumentException();
-                }
-                break;
-            case 2:
-                // TODO: Añadir el ingreso de clientes
-                break;
-            case 3:
-                // TODO: Añadir el ingreso de vendedores
-                break;
-            case 4:
-                // TODO: Añadir la salida de autos del patio
-                break;
-            case 5:
-                System.out.println("Gracias por confiar en nuestro sistema!");
-                System.exit(0);
-            default:
-                // En el imposible caso de que salga un número diferente (que ya validamos) sabremos porqué.
-                throw new IllegalArgumentException();
+        boolean menuPrincipal = true;
+        while (menuPrincipal) {
+            int respuesta = UI.imprimirMenuPrincipal();
+            switch (respuesta) {
+                case 1:
+                    UI.imprimirMenuIngresoVehicular();
+                    String tipo = UI.ingresarTipoVehiculo(tipos);
+                    switch (tipo) {
+                        case "automovil":
+                            ArrayList<Object> datos_comunes = UI.ingresoDatos();
+                            Automovil vh = UI.imprimirMenuIngresoAutomovil(datos_comunes);
+                            // Preguntar si quiere ingresar otro carro, sino regresa a menu principal.
+                            break;
+                        case "camionetas":
+                            // Terminar el menu de camionetas
+                            break;
+                        case "otros":
+                            // Terminar el menu para otros vehiculos
+                            break;
+                        default:
+                            // Asi vemos si las validaciones sirven.
+                            throw new IllegalArgumentException();
+                    }
+                    break;
+                case 2:
+                    // TODO: Añadir el ingreso de clientes
+                    break;
+                case 3:
+                    // TODO: Añadir el ingreso de vendedores
+                    break;
+                case 4:
+                    // TODO: Añadir la salida de autos del patio
+                    break;
+                case 5:
+                    System.out.println("Gracias por confiar en nuestro sistema!");
+                    menuPrincipal = false;
+                default:
+                    // En el imposible caso de que salga un número diferente (que ya validamos) sabremos porqué.
+                    throw new IllegalArgumentException();
+            }
+
         }
-
-
     }
 }
